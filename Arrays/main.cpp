63f204a4d3d40b8e3de02493c53b2835c9429f21
user <user@WIN-4KGTNI4DDSK>
2025-06-23 12:29:10 +0300

    Add project files.

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -6,7 +6,9 @@ using namespace std;
 //Single Responibility Principle - принцип единой ответственности.
 
 void FillRand(int arr[], const int n, int minRand = 0, int maxRand = 100);
+void FillRand(double arr[], const int n, int minRand = 0, int maxRand = 100);
 void Print(int arr[], const int n);
+void Print(double arr[], const int n);
 void Sort(int arr[], const int n);
 int Sum(int arr[], const int n);
 double Avarage(int arr[], const int n);
@@ -25,6 +27,11 @@ void main()
 	Print(arr, n);
 	cout << "Сумма элементов массива: " << Sum(arr, n) << endl;
 	cout << "Средне-Арифметическое элементов массива: " << Avarage(arr, n) << endl;
+
+	const int D_SIZE = 8;
+	double d_arr[D_SIZE];
+	FillRand(d_arr, D_SIZE);
+	Print(d_arr, D_SIZE);
 }
 #endif //ARRAYS
 
@@ -35,10 +42,25 @@ void FillRand(int arr[], const int n, int minRand, int maxRand)
 		arr[i] = minRand + rand() % (maxRand - minRand + 1);
 	}
 }
+void FillRand(double arr[], const int n, int minRand, int maxRand)
+{
+	for (int i = 0; i < n; i++) 
+	{
+		arr[i] = minRand + rand() % (maxRand - minRand + 1);
+	}
+}
 
 void Print(int arr[], const int n) 
 {
 	for (int i = 0; i < n; i++) 
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
+void Print(double arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << "\t";
 	}
@@ -85,5 +107,13 @@ void Print(int arr[], const int n);
 Print(arr, n);
 
 Любая функция для работы с массивом ... сам массив и его размер.
+
+Перегрузка функций назывется ситуация, когда есть 2 и более функции с одинаковым именем, но с разным списком приниаемым параметром.
+Список принимаемых параметров перегруженных функций может отличаться так и типом, как и кол-вом аргументов.
+Тип возращаемого значения с перегруженной функцией также может отличаться, но перегруженные функции не могут отличаться лишь типом возращаемого значения, поскольку компилятор определяет, какой экземпляр перегруженной функции нужно вызвать лишь по списку принимаемых параметров.
+Реализации перегруженной функции также может отличаться или быть одинаковым.
+Как правило, если список принимаемых параметров перегруженной функции отличаеться лишь типом аргументов, реализации таких функций будет одинковым или как минимум очень похожи.
+Но, если список принимаемых параметров перегруженной функци отличаеться кол-вом аргументов, то скорее всего и реализации таких функций будет разными.
+Если функция не умеет работать с каким-либо типом данных, то её нужно перегрузить для этого типа данных.
 
 */
