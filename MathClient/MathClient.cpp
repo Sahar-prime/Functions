d85ca1dc74097ba226859164de21b7a852b64b13
user <user@WIN-4KGTNI4DDSK>
2025-06-30 13:25:33 +0300

    Add project files.

--- MathClient/MathClient.cpp
+++ MathClient/MathClient.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main()
 {
-    double a = 7.4;
-    int b = 99;
+    double a = 5;
+    int b = 5;
 
     cout << "a + b = " <<
         MathLibrary::Arithmetic::Add(a, b) << endl;
@@ -13,6 +13,6 @@ int main()
         MathLibrary::Arithmetic::Subtract(a, b) << endl;
     cout << "a * b = " <<
         MathLibrary::Arithmetic::Multiply(a, b) << endl;
-    cout << "a / b = " <<
+    cout << "a : b = " <<
         MathLibrary::Arithmetic::Divide(a, b) << endl;
 }
